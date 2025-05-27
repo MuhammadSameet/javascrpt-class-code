480e283e3f9a32f8c5ca6c96971bb50023100c51
Muhammad Sameet <164825370+MuhammadSameet@users.noreply.github.com>
2025-05-27 10:47:37 +0500

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,35 @@
+// for (var i = 0; i < 10; i++) {
+//     console.log(i) // 1-9
+// };
+
+// for (i = 1 ; i <= 10; i++){
+//     console.log(i) //1-10
+// };
+
+
+// for (i = 1; i <= 12; i++){
+    //     console.log(i)
+    
+    //     if (i == 6){ // ab ya sirf 6 tak run hoga
+    //         break
+    //     }
+    // };
+
+
+
+var item = ['chips', 'cake', 'snaks', 'chocolate', 'biscuits']
+// var userinput = prompt("what do you want to eat")
+
+// if (item.indexOf(userinput) !== -1) {
+//     console.log(userinput, "is available in our bakery at index no ", item.indexOf(userinput));
+    
+// }
+// else {
+//     console.log(userinput, 'is not avaibale in our bakery');
+    
+// }
+
+
+for ( var i = 0 ; i <= item.length; i++){
+    console.log(i);
+};
