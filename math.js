a3687b7c6ac13e6b295d98ad1a72b049a2869d9c
Muhammad Sameet <164825370+MuhammadSameet@users.noreply.github.com>
2025-06-05 11:08:15 +0500

    Add files via upload

---
+++ math.js
@@ -0,0 +1,68 @@
+// var pivalue = Math.PI;
+// console.log(pivalue.toFixed(5));
+// console.log(pivalue.toFixed(3));
+// console.log(pivalue.toFixed(2));
+
+
+// round of value
+// console.log(Math.round(4)); //4
+// console.log(Math.round(4.1)); //4
+// console.log(Math.round(4.2)); //4
+// console.log(Math.round(4.3)); //4
+// console.log(Math.round(4.5)); // point kay baad 5 ya 5 say zada value ho tou value round of and greater than hogaye gi
+// console.log(Math.round(4.6)); //5
+// console.log(Math.round(4.7));  //5
+// console.log(Math.round(4.8)); //5
+// console.log(Math.round(4.9)); //5
+
+
+// abs absolute nagative value ko positive ma convert krdayta h , mean -4.7 ka 4 answer dayga
+
+// console.log(Math.abs(4.7));  //4.7
+// console.log(Math.abs(-4.8)); //4.8
+// console.log(Math.abs(-4.9)); //4.9
+// console.log(Math.abs(4.6)); //4.6
+
+// ceil bolta ha point kay baad jo bhi value hogi ma usko greater than say round figer krdonga maen 4.1 ko 5 krdayga
+
+// console.log(Math.ceil(4.7));  //5
+// console.log(Math.ceil(4.8)); //5
+// console.log(Math.ceil(4.9)); //5
+// console.log(Math.ceil(4.6)); //5
+
+
+// floor point kay baad wali value nhi dayta sirf value ko dayta h mean 4.2 ko 4
+
+// console.log(Math.floor(4.7));  //4
+// console.log(Math.floor(4.8)); //4
+// console.log(Math.floor(4.9)); //4
+// console.log(Math.floor(4.6)); //4
+
+
+//max bolta ha ma sirf sabsay bari value batanga
+
+// var a = Math.max(45, 56, 100, 101)
+// console.log(a); //101
+
+//min  bolta ha ma sirf sabsay choti value batanga
+
+// var b = Math.min(45, 56, 100, 101)
+// console.log(b); //45
+// var b = Math.min(-20, 56, 100, 101)
+// console.log(b); //-20
+
+
+//random koi bhi value khud say AUTO generate krta h
+// var d = Math.random();
+// console.log(d); // start generate value 0 point say hogi // like 0. something
+
+// var d = Math.random() * 7; //ab value 0 say 7 tak generate hogi like 0 to 7. something
+// console.log(d);
+
+// var m = Math.random() + 4; // ab ya sirf 4. something ka random number generate karenga
+// console.log(m);
+
+
+// var arr = [ 'taha', 'khushnoor', 'sameet', 'talha', 'junaid' ]
+
+// console.log(Math.random());
