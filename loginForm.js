720c04b30da975ef27611a0399b9341966324038
Muhammad Sameet <164825370+MuhammadSameet@users.noreply.github.com>
2025-07-19 11:19:04 +0500

    Add files via upload

--- loginForm.js
+++ loginForm.js
@@ -1,7 +1,9 @@
 var emailEl = document.getElementById("email")
-var emailContent = document.get
+var passwordEl = document.getElementById("password")
+var emailContent = document.createTextNode(emailEl).value
 console.log(emailContent);
 
 function loginFunc() {
-
+    console.log(emailContent);
+    
 }
