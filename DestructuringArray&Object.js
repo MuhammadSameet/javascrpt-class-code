f2f0e2f9f49e7fdb669f8a989287d4c6b908956e
Muhammad Sameet <164825370+MuhammadSameet@users.noreply.github.com>
2025-07-19 11:18:43 +0500

    Add files via upload

---
+++ DestructuringArray&Object.js
@@ -0,0 +1,44 @@
+
+// var user = ['john' , 'taha', 'mohamed', 'ahmed', 'ali'];
+
+// for (let i in user) { // in "index number ki Value/content dayta h"
+//     console.log(i); // ya number dayga
+//     console.log(user[i]); // ya value/content dayga
+// }
+
+// for (let i of user) { // of index number dayta h
+//     console.log(i);
+//     //console.log(user[i]); // work nhi karay ga
+// }
+
+// var input = Number(prompt("Enter your Marks: "));
+
+// if (input == 80) console.log("First class student");
+// else if (input == 50) console.log("second class student");
+// else console.log("third class student");
+
+// var arr = ['taha', 'mohamed', 'ahmed', 'ali']
+
+// for (let i in arr) {
+//     console.log(i);
+//     console.log(arr[i]);
+// }
+
+// for (let i of arr) {
+//     console.log(i); // in "index number ki Value/content dayta h // in only index nuber dayta h"
+//     console.log(arr[i]) // undefine aiga, work nh karega
+// }
+
+// var obj = {
+//     name: 'taha',
+//     fname : "ameen",
+//     contactNo : 1234567890
+// }
+
+// for (let i in obj) {
+//     // console.log(obj); // full object dayga
+//     // console.log(i); // property ki name dayga
+//     // console.log(i , obj[i]); // property ka name , property ki value dayga
+//     console.log(obj.fname); // property ki value dayga
+// }
+
